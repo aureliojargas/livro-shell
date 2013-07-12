f4c93b8afdfc5a2ddd354e8ef8120483a8b3ae5b
Aurelio Jargas <verde@aurelio.net>
2013-07-12 15:26:02 -0300

    Caracteres de controle trocados por escapes
    Na primeira impressão do livro foi usada a notação conservadora para os
    caracteres de controle, com Esc literal ^[ (Ctrl-V, Esc) e bipe como ^G
    (Ctrl-V, Ctrl-G).
    
    Mas não faz sentido usar esta notação arcaica. Em sistemas modernos,
    prefira esta notação, com \033 (Esc) e \a (bipe).
    
    Bug reportado por Eliphas Levy Theodoro.

--- 06-caracteres/sirene.sh
+++ 06-caracteres/sirene.sh
@@ -2,10 +2,9 @@
 # sirene.sh
 # Bom para chamar a atenção dos colegas de trabalho :)
 
-echo -n "[11;900]"   # Cada bipe dura quase um segundo
+echo -ne "\033[11;900]"   # Cada bipe dura quase um segundo
 while :                   # Loop eterno
 do
-	echo -n "[10;500]" ; sleep 1  # Tom alto (agudo)
-	echo -n "[10;400]" ; sleep 1  # Tom baixo (grave)
+	echo -ne "\033[10;500]\a" ; sleep 1  # Tom alto (agudo)
+	echo -ne "\033[10;400]\a" ; sleep 1  # Tom baixo (grave)
 done
-
